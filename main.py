04e6e0f2f6a7cfa51520c748fd46e02e69db2e21
dennis <abowlofmilchreis@gmail.com>
2022-12-26 14:15:08 +0100

    1st working version

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from clint.textui import progress
+from zipfile import ZipFile
+import shutil
 
 def getVersion():
     AppPath = os.path.expanduser('~') + "/AppData/Local/Chromium/Application/"
@@ -24,27 +26,58 @@ def downloadLatestVersion(version):
     baseUrl = "https://ungoogled-software.github.io/ungoogled-chromium-binaries/releases/windows/64bit/"
     fullUrl = baseUrl + version
 
+    print(fullUrl)
+
     r = requests.get(fullUrl)
     soup = BeautifulSoup(r.content, 'html.parser')
-    installerUrl = soup.find_all("a")[6]["href"]
+    archiveUrl = soup.find_all("a")[7]["href"]
     
-    r = requests.get(installerUrl, stream=True)
-    FileName = installerUrl.split("/")[-1]
+
+    r = requests.get(archiveUrl, stream=True)
+    FileName = archiveUrl.split("/")[-1]
     print(FileName)
-    with open(FileName, "wb") as Installer:
+    FilePath = os.getcwd() + "/tmp/" + FileName
+    
+    
+    with open(FilePath, "wb") as BrowserArchive:
         total_length = int(r.headers.get('content-length'))
         for ch in progress.bar(r.iter_content(chunk_size = 2391975), expected_size=(total_length/1024) + 1):
             if ch:
-                Installer.write(ch)
+                BrowserArchive.write(ch)
 
 
     return FileName
 
 
+def updateFiles(Version, latestVersion, ArchiveName):
+    AppPath = os.path.expanduser('~') + "/AppData/Local/Chromium/Application/"
+    AppDir = os.path.join(AppPath, Version)
+    #os.rmdir(AppDir)
 
+    ZipArchiveDestinationDirectory = os.path.join(AppPath, latestVersion.split("-")[0])
+    ZipArchive = ZipFile(os.path.join(os.path.join(os.getcwd(), "tmp"), ArchiveName))
+    ZipArchive.extractall(path=os.path.join(os.getcwd(), "tmp"))
+
+    
+
+    os.rename(os.path.join(os.path.join(os.getcwd(), "tmp"), os.listdir(os.path.join(os.getcwd(), "tmp"))[0]), ZipArchiveDestinationDirectory)
+
+    WidevineCdmPath = os.getcwd() + "/resources/WidevineCdm"
+    WidevineCdmDestinationPath = os.path.join(ZipArchiveDestinationDirectory, "WidevineCdm")
+    shutil.copytree(WidevineCdmPath, WidevineCdmDestinationPath)
+
+
+
+
+os.mkdir(os.path.join(os.getcwd(), "tmp"), 0o666)
 Version = getVersion()
 latestVersion = getLatestVersion()
 
 if Version != latestVersion[:-2]:
-    InstallerFile = downloadLatestVersion(latestVersion)
-    os.system(f".\{InstallerFile}")
+    ArchiveName = downloadLatestVersion(latestVersion)
+    updateFiles(Version=Version, latestVersion=latestVersion, ArchiveName=ArchiveName)
+
+else:
+    print(f"The latest version ({Version}) is installed!")
+
+shutil.rmtree(os.path.join(os.getcwd(), "tmp"))
