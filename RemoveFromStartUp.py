c772a487927af7531ee4192d1891b3aeef8cce13
dennis <abowlofmilchreis@gmail.com>
2022-12-26 20:03:47 +0100

    remove tool from windows startup

--- RemoveFromStartUp.py
+++ RemoveFromStartUp.py
@@ -1,9 +1,14 @@
 import os
 from readchar import readchar
+import pyfiglet
+import pyfiglet.fonts
 
 AUTOSTART_PATH = os.path.expanduser('~') + r"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AutoUpdater.lnk"
 os.remove(AUTOSTART_PATH)
 
+f = pyfiglet.Figlet(font='slant')
+print(f.renderText('Ungoogled Chromium Autoupdater'))
+print()
 print("The Script Has Been Removed From The Startup!")
 print("Press Any Key To Exit...")
 readchar()
