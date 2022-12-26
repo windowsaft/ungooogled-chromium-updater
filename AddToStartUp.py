52e5109fc40b267700ab955647f71b89de7b499d
dennis <abowlofmilchreis@gmail.com>
2022-12-26 20:03:33 +0100

    add tool to windows startup

--- AddToStartUp.py
+++ AddToStartUp.py
@@ -1,12 +1,12 @@
 import os
 from win32com.client import Dispatch
 from readchar import readchar
+import pyfiglet
+import pyfiglet.fonts
 
-PATH = os.path.dirname(os.path.realpath(__file__))
+PATH = os.getcwd()
 AUTOSTART_PATH = os.path.expanduser('~') + r"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AutoUpdater.lnk"
-TARGET_PATH = os.path.join(PATH, "main.exe")
-
-print(AUTOSTART_PATH)
+TARGET_PATH = os.path.join(PATH, "AutoUpdater.exe")
 
 shell = Dispatch('WScript.Shell')
 shortcut = shell.CreateShortCut(AUTOSTART_PATH)
@@ -14,6 +14,9 @@ shortcut.Targetpath = TARGET_PATH
 shortcut.WorkingDirectory = PATH
 shortcut.save()
 
+f = pyfiglet.Figlet(font='slant')
+print(f.renderText('Ungoogled Chromium Autoupdater'))
+print()
 print("The Script Has Been Added To The Startup!")
 print("Press Any Key To Exit...")
 readchar()
